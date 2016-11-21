40eaa96aa1216a3531e437156c644b6ba401bcbd
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2016-11-21 23:43:11 +0100

    release with mincss

--- release.sh
+++ release.sh
@@ -10,3 +10,4 @@ lein cljsbuild once min || exit 1
 cp $RESURCES/index.html $DIST/ || exit 1
 cp $RESURCES/fonts $DIST/ -r || exit 1
 cp $RESURCES/css $DIST/ -r || exit 1
+cleancss $RESURCES/css/main.css -o $DIST/css/main.css || exit 1
